dedba9556478c9983dd0347fc74116ea208903be
Matt Watson <matthew.watson@qa.com>
2016-08-09 22:54:38 +0100

    installation and code ran

--- manifests/go.pp
+++ manifests/go.pp
@@ -10,5 +10,12 @@ class profile::go (
   package { 'golang':
       ensure  => $version,
   }
+  Class['common::files'] ->
+  exec { 'install go code':
+    command => '( cd $HOME/gocode/src/basic/ && go install)',
+  } ->
+  exec { 'run go code':
+    command => '$HOME/gocode/src/basic/basic',
+  }
 
 }
