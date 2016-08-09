120aa7787134893136f830c708664b38ab8712de
Matt Watson <matthew.watson@qa.com>
2016-08-09 21:54:02 +0100

    default nginx stopped

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -2,13 +2,16 @@
 #
 #
 class profile::nginx (
-  $nginx_hosts = {},
+  $nginx_hosts   = {},
+  $nginx_service = 'stopped',
 ){
-    include ::nginx
+  class { '::nginx':
+    service_ensure => $nginx_service,
+  }
 
-    if $nginx_hosts != undef {
-      validate_hash($nginx_hosts)
-      create_resources(common::host,$nginx_hosts)
-    }
+  if $nginx_hosts != undef {
+    validate_hash($nginx_hosts)
+    create_resources(common::host,$nginx_hosts)
+  }
 
 }
