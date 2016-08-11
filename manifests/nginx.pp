33392074737b6dc5ccefb9ccc32bd64725626b9a
Matt Watson <matthew.watson@qa.com>
2016-08-12 00:32:58 +0100

    ordering changed

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -3,24 +3,21 @@
 #
 class profile::nginx (
   $nginx_hosts   = {},
-  $nginx_service = stopped,
-  $consul_template_service = running
 ){
 
   include ::consul
-
-  Class['::consul'] ->
-  class { '::nginx':
-    service_ensure => $nginx_service,
-  } ->
-  class { '::consul_template':
-    service_ensure => $consul_template_service,
-    notify         => Nginx::Service[$name],
-  }
+  include ::consul_template
+  include ::nginx
 
   if $nginx_hosts != undef {
     validate_hash($nginx_hosts)
     create_resources(common::host,$nginx_hosts)
+  }
+
+  $consul_template_watches = hiera_hash('consul_template::watches')
+
+  if $consul_template_watches {
+    create_resources('consul_template::watch', $consul_template_watches)
   }
 
 }
