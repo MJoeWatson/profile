d7d18067a4fb60f21f8c4a0fff91ff447f1f46f6
Matt Watson <matthew.watson@qa.com>
2016-08-11 20:23:12 +0100

    consul and consul_templates added

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -9,6 +9,9 @@ class profile::nginx (
     service_ensure => $nginx_service,
   }
 
+  include ::consul
+  include ::consul_template
+
   if $nginx_hosts != undef {
     validate_hash($nginx_hosts)
     create_resources(common::host,$nginx_hosts)
