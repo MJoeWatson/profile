ccbfc4056e2003c4b1826503051d5cd47e7fac03
Matt Watson <matthew.watson@qa.com>
2016-08-12 01:32:33 +0100

    remove consul watch look up

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -8,18 +8,12 @@ class profile::nginx (
   include ::consul_template
   include ::nginx
   include ::common
-  Class['::common'] -> Class['::nginx']
+  Class['::common'] -> Class['::nginx'] -> Class['::consul_template']
 
   $nginx_hosts = hiera_hash('nginx_hosts', {})
   if $nginx_hosts != undef {
     validate_hash($nginx_hosts)
     create_resources(common::host,$nginx_hosts)
-  }
-
-  $consul_template_watches = hiera_hash('consul_templates::watches')
-
-  if $consul_template_watches {
-    create_resources('consul_template::watch', $consul_template_watches)
   }
 
 }
